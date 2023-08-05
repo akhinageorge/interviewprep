aaa7dcd570965403efb2e5c40709e23edddaec69
Akhina George <89385646+akhinageorge@users.noreply.github.com>
2023-08-05 21:40:46 +0530

    Day1.py

--- questions.py
+++ questions.py
@@ -24,8 +24,30 @@ def plusMinus(arr):
     print(pos/n)
     print(neg/n)
     print(zero/n)
-          
+    
 if __name__ == '__main__':
     n = int(input().strip())
     arr = list(map(int, input().rstrip().split()))
     plusMinus(arr)
+
+#Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+def timeConversion(s):
+    hours, minutes, seconds = map(int, s[:-2].split(':'))
+    meridian = s[-2:]
+
+    if meridian == "AM":
+        if hours == 12:
+            hours = 0
+    elif meridian == "PM":
+        if hours != 12:
+            hours += 12
+
+    military_time = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+    return military_time
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = timeConversion(s)
+    fptr.write(result + '\n')
+    fptr.close()
